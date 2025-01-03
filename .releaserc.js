3293a081686289d640fb7d376f2ff3e660a49692
杨成锴 <homeboyc@foxmail.com>
2025-01-03 20:37:59 +0800

    fix: fix branch issue

--- .releaserc.js
+++ .releaserc.js
@@ -1,9 +1,17 @@
 // From: https://github.com/asjdf/semantic-release-go-demo
 module.exports = {
-    branches: ["main", {name: 'alpha', prerelease: true}, {name: 'beta', prerelease: true}],
+    branches: [
+        "+([0-9])?(.{+([0-9]),x}).x",
+        "master",
+        "main",
+        "next",
+        "next-major",
+        { name: "beta", prerelease: true },
+        { name: "alpha", prerelease: true },
+    ],
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        "@semantic-release/github"
-    ]
+        "@semantic-release/github",
+    ],
 };
