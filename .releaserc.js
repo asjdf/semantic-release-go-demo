dbb8a2369d69448de46bd9ff1268254833c83023
杨成锴 <homeboyc@foxmail.com>
2025-03-02 13:58:19 +0800

    fix: tmp fix rate-limit of GitHub

--- .releaserc.js
+++ .releaserc.js
@@ -12,6 +12,13 @@ module.exports = {
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        "@semantic-release/github",
+        [
+            "@semantic-release/github",
+            {
+                successComment: false, // tmp fix https://github.com/semantic-release/github/pull/857
+                failComment: false,
+                failTitle: false,
+            },
+        ],
     ],
 };
